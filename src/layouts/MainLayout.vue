54e0a3592c66aa3c843dd8943fd516e43b721304
Jorge Barcos <jorgebarcos88@gmail.com>
2021-06-06 19:02:21 -0300

    Right Drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,34 +10,94 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      show-if-above
-      v-model="left"
-      :width="283"
-      side="left"
-      bordered>
+    <q-drawer show-if-above v-model="left" :width="283" side="left" bordered>
       <q-icon class="q-pa-md" name="fas fa-dove" size="lg" color="primary" />
 
       <q-list>
         <q-item clickable v-ripple to="/">
           <q-item-section avatar>
-            <q-icon  name="home" size="md" />
+            <q-icon name="home" size="md" />
           </q-item-section>
 
           <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
         </q-item>
-         <q-item clickable v-ripple to="/about">
+        <q-item clickable v-ripple to="/about">
           <q-item-section avatar>
-            <q-icon  name="help" size="md" />
+            <q-icon name="help" size="md" />
           </q-item-section>
 
-          <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+          <q-item-section class="text-h6 text-weight-bold"
+            >About</q-item-section
+          >
         </q-item>
       </q-list>
     </q-drawer>
 
     <q-drawer show-if-above v-model="right" side="right" bordered>
-      <!-- drawer content -->
+      <q-input
+        class="q-ma-md"
+        outlined
+        rounded
+        placeholder="Search Qwitter"
+        dense
+      >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
+      <q-list padding separator>
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey">Education</q-item-label>
+            <q-item-label class="text-weight-bold"
+              >Something amazing happended!</q-item-label
+            >
+            <q-item-label caption
+              >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+              adipiscit elit.</q-item-label
+            >
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey">Education</q-item-label>
+            <q-item-label class="text-weight-bold"
+              >Something amazing happended!</q-item-label
+            >
+            <q-item-label caption
+              >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+              adipiscit elit.</q-item-label
+            >
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey">Education</q-item-label>
+            <q-item-label class="text-weight-bold"
+              >Something amazing happended!</q-item-label
+            >
+            <q-item-label caption
+              >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+              adipiscit elit.</q-item-label
+            >
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-page-container>
