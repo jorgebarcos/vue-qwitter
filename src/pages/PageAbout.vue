07c8e3f57931c0eac9886c58a14f3e8c221ab110
Jorge Barcos <jorgebarcos88@gmail.com>
2021-06-06 19:27:08 -0300

    About Page

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,6 +1,20 @@
 <template>
-  <q-page>
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim facere,
+        excepturi autem totam repellendus consequuntur sequi commodi nulla
+        voluptates quas ipsa asperiores eum odit at ipsum in esse eos
+        accusantium.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim facere,
+        excepturi autem totam repellendus consequuntur sequi commodi nulla
+        voluptates quas ipsa asperiores eum odit at ipsum in esse eos
+        accusantium.
+      </p>
+    </div>
   </q-page>
 </template>
 
