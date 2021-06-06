b24f8d7c6c13e7d38716ceb6e9d645951fb95e46
Jorge Barcos <jorgebarcos88@gmail.com>
2021-06-06 20:05:00 -0300

    New Tweet form

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,11 +1,60 @@
 <template>
   <q-page>
-    <h5>Home</h5>
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <div class="col">
+        <q-input
+          bottom-slots
+          v-model="newQweetContent"
+          class="new-qweet"
+          placeholder="What's happening?"
+          counter
+          maxlength="280"
+          autogrow
+        >
+          <template v-slot:before>
+            <q-avatar size="xl">
+              <img
+                src="https://es.gravatar.com/userimage/140485787/ded6e4a586fac7c16cbe46c7358b45bf.jpg?size=200"
+              />
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
+      <div class="col col-shrink">
+        <q-btn
+          color="primary"
+          label="Qweet"
+          rounded
+          unelevated
+          no-caps
+          :disable="newQweetContent"
+          class="q-mb-lg"
+        />
+      </div>
+    </div>
+
+    <q-separator size="10px" color="grey-2" class="divider" />
   </q-page>
 </template>
 
 <script>
 export default {
-  name: "PageHome"
+  name: "PageHome",
+  data() {
+    return {
+      newQweetContent: ""
+    };
+  }
 };
 </script>
+
+<style lang="sass">
+.new-qweet
+  textarea
+    font-size: 19px
+    line-height: 1.4 !important
+.divider
+  border-top: 1px solid
+  border-bottom: 1px solid
+  border-color: $grey-4
+</style>
