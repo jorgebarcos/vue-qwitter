3515a6d54f6c094b04fd0618846939150be5dfbc
Jorge Barcos <jorgebarcos88@gmail.com>
2021-06-07 09:23:11 -0300

    Animation

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -37,42 +37,48 @@
     <q-separator size="10px" color="grey-2" class="divider" />
 
     <q-list separator>
-      <q-item class="q-py-md" v-for="qweet in qweets" :key="qweet.date">
-        <q-item-section avatar top>
-          <q-avatar size="xl">
-            <img
-              src="https://es.gravatar.com/userimage/140485787/ded6e4a586fac7c16cbe46c7358b45bf.jpg?size=200"
-            />
-          </q-avatar>
-        </q-item-section>
+      <transition-group
+        appear
+        enter-active-class="animated fadeIn slow"
+        leave-active-class="animated fadeOut slow"
+      >
+        <q-item class="q-py-md qweet" v-for="qweet in qweets" :key="qweet.date">
+          <q-item-section avatar top>
+            <q-avatar size="xl">
+              <img
+                src="https://es.gravatar.com/userimage/140485787/ded6e4a586fac7c16cbe46c7358b45bf.jpg?size=200"
+              />
+            </q-avatar>
+          </q-item-section>
 
-        <q-item-section>
-          <q-item-label class="text-subtitle1"
-            ><strong>Jorge Barcos</strong>
-            <span class="text-grey-7">@jorge__barcos</span>
-          </q-item-label>
-          <q-item-label class="qweet-content text-body1">
-            {{ qweet.content }}
-          </q-item-label>
-          <div class="qweet-icons row justify-between q-mt-sm">
-            <q-btn color="grey" icon="far fa-comment" size="sm" flat round />
-            <q-btn color="grey" icon="fas fa-retweet" size="sm" flat round />
-            <q-btn color="grey" icon="far fa-heart" size="sm" flat round />
-            <q-btn
-              color="grey"
-              icon="fas fa-trash"
-              size="sm"
-              flat
-              round
-              @click="deleteQweet(qweet)"
-            />
-          </div>
-        </q-item-section>
+          <q-item-section>
+            <q-item-label class="text-subtitle1"
+              ><strong>Jorge Barcos</strong>
+              <span class="text-grey-7">@jorge__barcos</span>
+            </q-item-label>
+            <q-item-label class="qweet-content text-body1">
+              {{ qweet.content }}
+            </q-item-label>
+            <div class="qweet-icons row justify-between q-mt-sm">
+              <q-btn color="grey" icon="far fa-comment" size="sm" flat round />
+              <q-btn color="grey" icon="fas fa-retweet" size="sm" flat round />
+              <q-btn color="grey" icon="far fa-heart" size="sm" flat round />
+              <q-btn
+                color="grey"
+                icon="fas fa-trash"
+                size="sm"
+                flat
+                round
+                @click="deleteQweet(qweet)"
+              />
+            </div>
+          </q-item-section>
 
-        <q-item-section side top>
-          {{ qweet.date | relativeDate }}
-        </q-item-section>
-      </q-item>
+          <q-item-section side top>
+            {{ qweet.date | relativeDate }}
+          </q-item-section>
+        </q-item>
+      </transition-group>
     </q-list>
   </q-page>
 </template>
@@ -105,6 +111,7 @@ export default {
         date: Date.now()
       };
       this.qweets.unshift(newQweet);
+      this.newQweetContent = "";
     },
     deleteQweet(qweet) {
       let dateToDelete = qweet.date;
@@ -129,6 +136,8 @@ export default {
   border-top: 1px solid
   border-bottom: 1px solid
   border-color: $grey-4
+.qweet:not(:first-child)
+  border-top: 1px solid rgba(0, 0, 0, 0.12)
 .qweet-content
   white-space: pre-line
 .qweet-icons
